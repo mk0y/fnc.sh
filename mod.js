b4fb8fd157350edce834be79f2a7a6e08aa31180
Marko Jakic <jakicmeister@gmail.com>
2021-10-01 11:33:17 +0200

    try Function

--- mod.js
+++ mod.js
@@ -6,6 +6,16 @@ addEventListener("fetch", (event) => {
   
   const fn = '(x, y) => x + y';
 
+  const func = new Function('x', 'y', 'return x + y;');
+
+  // func.bind(null, [2, 3]);
+
+  console.log(func.toString());
+
+  const res = func(1, 2);
+
+  console.log(res);
+
   const resp = new Response("Hello2", {
     headers: { "content-type": "text/plain" },
   });
